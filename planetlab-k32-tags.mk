dc9ea533d8e03d98d805f851167083a86798a979
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-01-09 09:27:57 +0100

    Setting tag sface-0.9-3 handle blank rspecs

--- planetlab-k32-tags.mk
+++ planetlab-k32-tags.mk
@@ -45,7 +45,7 @@ ejabberd-GITPATH		:= git://git.planet-lab.org/ejabberd.git@ejabberd-2.1.6-2
 omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-11
 ###
 sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@sfa-2.0-7
-sface-GITPATH                   := git://git.planet-lab.org/sface.git@sface-0.9-2
+sface-GITPATH                   := git://git.planet-lab.org/sface.git@sface-0.9-3
 nodeconfig-GITPATH		:= git://git.planet-lab.org/nodeconfig.git@nodeconfig-5.0-5
 bootmanager-GITPATH             := git://git.planet-lab.org/bootmanager.git@bootmanager-5.0-20
 pypcilib-GITPATH		:= git://git.planet-lab.org/pypcilib.git@pypcilib-0.2-10
