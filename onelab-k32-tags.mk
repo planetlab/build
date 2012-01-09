64785b25d8e0606bd7d711ba32af764148cb3922
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-01-09 09:31:24 +0100

    sfa-2.0-9

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -49,7 +49,7 @@ ejabberd-GITPATH                := git://git.onelab.eu/ejabberd.git@master
 omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-11
 oml-GITPATH                     := git://git.onelab.eu/oml.git@oml-2.6.1-1
 ###
-sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-2.0-8
+sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-2.0-9
 sface-GITPATH                   := git://git.onelab.eu/sface.git@sface-0.9-3
 nodeconfig-GITPATH              := git://git.onelab.eu/nodeconfig.git@nodeconfig-5.0-5
 bootmanager-GITPATH             := git://git.onelab.eu/bootmanager.git@bootmanager-5.0-20
