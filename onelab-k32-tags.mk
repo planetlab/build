2bc8408972caea15575a8df949994bb797ee85ec
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-02-23 18:26:45 +0100

    point to git.onelab.eu

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -2,7 +2,7 @@
 
 ###
 linux-2.6-BRANCH		:= 32
-linux-2.6-GITPATH		:= git://git.planet-lab.org/linux-2.6.git@linux-2.6-32-27
+linux-2.6-GITPATH		:= git://git.onelab.eu/linux-2.6.git@linux-2.6-32-27
 # help out spec2make on f8 and centos5, due to a bug in rpm 
 # ditto on f16 for spec2make.py - tmp hopefully
 ifeq "$(DISTRONAME)" "$(filter $(DISTRONAME),f8 f16 centos5)"
