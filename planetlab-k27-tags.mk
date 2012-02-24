ec2f7fe2d7f0b71a43f3431df513ec982132c493
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-02-24 18:39:24 +0100

    Setting tag sfa-2.1-3 slice x researcher rel. in database, plimporter to maintain that, as well as user.email, and more robust ongoing draft for sfaadmin tool support for a federica driver support for a nova/euca driver no more sfa-clean-peer-records script

--- planetlab-k27-tags.mk
+++ planetlab-k27-tags.mk
@@ -36,7 +36,7 @@ pyaspects-GITPATH		:= git://git.planet-lab.org/pyaspects.git@pyaspects-0.4.1-2
 ejabberd-GITPATH		:= git://git.planet-lab.org/ejabberd.git@ejabberd-2.1.6-3
 omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-11
 ###
-sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@sfa-2.0-7
+sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@sfa-2.1-3
 nodeconfig-GITPATH		:= git://git.planet-lab.org/nodeconfig.git@nodeconfig-5.0-6
 pypcilib-GITPATH		:= git://git.planet-lab.org/pypcilib.git@pypcilib-0.2-10
 pyplnet-GITPATH                 := git://git.planet-lab.org/pyplnet.git@pyplnet-4.3-11
