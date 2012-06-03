955c86fcb7372f2697a845c46d808bf875906555
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-06-03 15:28:58 +0200

    Setting tag sfa-2.1-9 fix broken sfa.util.xrn class for lowercase

--- planetlab-k32-tags.mk
+++ planetlab-k32-tags.mk
@@ -43,7 +43,7 @@ pyopenssl-GITPATH               := git://git.planet-lab.org/pyopenssl.git@pyopen
 pyaspects-GITPATH		:= git://git.planet-lab.org/pyaspects.git@pyaspects-0.4.1-2
 omf-GITPATH                     := git://git.onelab.eu/omf.git@omf-5.3-11
 ###
-sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@sfa-2.1-8
+sfa-GITPATH                     := git://git.planet-lab.org/sfa.git@sfa-2.1-9
 sface-GITPATH                   := git://git.planet-lab.org/sface.git@sface-0.9-5
 nodeconfig-GITPATH		:= git://git.planet-lab.org/nodeconfig.git@nodeconfig-5.0-7
 bootmanager-GITPATH             := git://git.planet-lab.org/bootmanager.git@bootmanager-5.0-22
