f3bd5d4492c24ac0aef1291b08ea531cc7ca66af
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-06-15 17:04:30 +0300

    Setting tag ipfw-20120610-1 integrated ipfw3 as of 20120610 from upstream

--- onelab-k32-tags.mk
+++ onelab-k32-tags.mk
@@ -12,7 +12,7 @@ kernel-DEVEL-RPMS		+= elfutils-libelf-devel
 
 madwifi-GITPATH                 := git://git.onelab.eu/madwifi.git@madwifi-4132-5
 iptables-GITPATH                := git://git.onelab.eu/iptables.git@iptables-1.4.10-5
-ipfw-GITPATH                    := git://git.onelab.eu/ipfw.git@master
+ipfw-GITPATH                    := git://git.onelab.eu/ipfw.git@ipfw-20120610-1
 ###
 comgt-SVNPATH			:= http://svn.onelab.eu/comgt/imports/0.3
 planetlab-umts-tools-GITPATH    := git://git.onelab.eu/planetlab-umts-tools.git@planetlab-umts-tools-0.6-6
