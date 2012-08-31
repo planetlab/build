026a6adb149d0520c85826478ad5aafe7ea88410
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-08-31 19:43:54 +0200

    Setting tag myplc-5.1-4 set TimeoutSec to 300 in plc.service remove ref to deprecated svn $URL$ in db-config usage

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -55,7 +55,7 @@ pyplnet-GITPATH                 := git://git.onelab.eu/pyplnet.git@pyplnet-4.3-1
 bootcd-GITPATH                  := git://git.onelab.eu/bootcd.git@bootcd-5.1-2
 sliceimage-GITPATH              := git://git.onelab.eu/sliceimage.git@sliceimage-5.1-2
 nodeimage-GITPATH               := git://git.onelab.eu/nodeimage.git@nodeimage-2.1-1
-myplc-GITPATH                   := git://git.onelab.eu/myplc.git@master
+myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.1-4
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
 
 # locating the right test directory - see make tests_gitpath
