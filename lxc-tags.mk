5284421165aaa0ae3cd9a32b563bc7a5907cd034
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2012-11-26 10:04:07 +0100

    Setting tag bind_public-0.1-2 bug fixes

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -23,7 +23,7 @@ openvswitch-GITPATH		:= git://git.planet-lab.org/openvswitch.git@master
 vsys-GITPATH			:= git://git.planet-lab.org/vsys.git@vsys-0.99-2
 vsys-scripts-GITPATH		:= git://git.planet-lab.org/vsys-scripts@vsys-scripts-0.95-44
 # somehow this won't mirror
-bind_public-GITPATH		:= git://git.onelab.eu/bind_public.git@master
+bind_public-GITPATH             := git://git.onelab.eu/bind_public.git@bind_public-0.1-2
 plcapi-GITPATH                  := git://git.onelab.eu/plcapi.git@master
 drupal-GITPATH                  := git://git.planet-lab.org/drupal.git@drupal-4.7-15
 plewww-GITPATH                  := git://git.planet-lab.org/plewww.git@master
