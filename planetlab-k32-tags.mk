129a9cdd15ebbd841140f3a12d67efeab40b3811
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2012-12-19 08:14:18 +0100

    Setting tag myplc-5.1-5 define open_basedir in php.ini to stop confidentiality leak new utility slice_ssh_keys.py for showing sliver keys (OMF interop) new config setting PLC_VSYS_DEFAULTS for vsys tags granted to all new utilities check-hrns.py and check-vsys-defaults.py

--- planetlab-k32-tags.mk
+++ planetlab-k32-tags.mk
@@ -55,6 +55,6 @@ pcucontrol-GITPATH              := git://git.planet-lab.org/pcucontrol.git@pcuco
 bootcd-GITPATH                  := git://git.planet-lab.org/bootcd.git@master
 sliceimage-GITPATH              := git://git.planet-lab.org/sliceimage.git@master
 nodeimage-GITPATH               := git://git.planet-lab.org/nodeimage.git@master
-myplc-GITPATH                   := git://git.planet-lab.org/myplc.git@myplc-5.1-4
+myplc-GITPATH                   := git://git.planet-lab.org/myplc.git@myplc-5.1-5
 # locating the right test directory - see make tests_gitpath
 tests-GITPATH                   := git://git.planet-lab.org/tests.git@tests-5.1-8
