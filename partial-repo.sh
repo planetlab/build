5f312750e0717b09251bdb7d80b763e7bcd8b17d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-02-08 10:12:01 +0100

    fix

--- partial-repo.sh
+++ partial-repo.sh
@@ -13,13 +13,13 @@ case "$1" in *-h*) usage ;; esac
 
 set -e 
 
-for rpms in $(find "$@" -name RPMS) ; do
-    cd $repo
+for rpms_dir in $(find "$@" -name RPMS) ; do
+    cd $rpms_dir
     cd ..
     echo "==================== Dealing with repo $(pwd)"
     mkdir -p PARTIAL-RPMS
     rsync --archive --verbose $(find RPMS -type f | egrep '/(bootcd|bootstrapfs|nodeimage|noderepo|slicerepo)-.*-.*-.*-.*rpm') PARTIAL-RPMS/
-    echo "==================== building packages index in $repo .."
+    echo "==================== building packages index in $(pwd) .."
     createrepo PARTIAL-RPMS
     echo '==================== DONE'
     cd - >& /dev/null
