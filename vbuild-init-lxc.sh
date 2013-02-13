74545be5f7037f0a5b1e4ade74d5a1b28014d86b
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-02-13 09:42:27 +0100

    turning off getty is uncredibly hard

--- vbuild-init-lxc.sh
+++ vbuild-init-lxc.sh
@@ -273,7 +273,8 @@ function configure_fedora_systemd() {
 #   #dependency on a device unit fails it specially that we disabled udev
 #    sed -i 's/After=dev-%i.device/After=/' ${rootfs_path}/lib/systemd/system/getty\@.service
     ln -s /dev/null ${rootfs_path}/etc/systemd/system/"getty@.service"
-    ln -s /dev/null ${rootfs_path}/usr/lib/systemd/system/"getty@.service"
+    rm /etc/systemd/system/getty.target.wants/*service
+# can't seem to handle this one with systemctl
     chroot ${rootfs_path} chkconfig network on
 }
 
