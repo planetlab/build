253a44706af0b9729f046c2d6d5eac89712026dd
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-04-23 17:04:20 +0200

    use tagged version of ipfw

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -10,8 +10,7 @@ kernel-WHITELIST-RPMS	:= kernel-devel,kernel-headers
 endif
 kernel-DEVEL-RPMS		+= elfutils-libelf-devel
 
-#ipfw-GITPATH                    := git://git.onelab.eu/ipfw.git@ipfw-20120610-2
-ipfw-GITPATH                    := git://git.code.sf.net/p/dummynet/code@master
+ipfw-GITPATH                    := git://git.code.sf.net/p/dummynet/code@ipfw-20130423-1
 madwifi-GITPATH                 := git://git.onelab.eu/madwifi.git@madwifi-4132-6
 iptables-GITPATH                := git://git.onelab.eu/iptables.git@iptables-1.4.10-5
 ###
