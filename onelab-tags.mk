c1ecc48a22c4ac924fe86378915d8a7d4d906984
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-04-24 16:47:48 +0200

    tests master for default on lxc

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -63,4 +63,4 @@ sface-GITPATH                   := git://git.onelab.eu/sface.git@sface-0.9-9
 myslice-GITPATH			:= git://git.onelab.eu/myslice-django.git@master
 manifold-GITPATH		:= git://git.top-hat.info/tophat/repositories/tophat.git@packaging
 # locating the right test directory - see make tests_gitpath
-tests-GITPATH                   := git://git.planet-lab.org/tests.git@tests-5.2-2
+tests-GITPATH                   := git://git.planet-lab.org/tests.git@master
