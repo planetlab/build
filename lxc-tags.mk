8d770feb8e840566d010dd21b6b905f751559af3
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2013-05-22 08:19:27 +0200

    use tagged procprotect

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -3,7 +3,7 @@
 lxcsu-GITPATH			:= git://git.planet-lab.org/lxcsu.git@lxcsu-0.2-1
 lxctools-GITPATH                := git://git.planet-lab.org/lxctools.git@lxctools-0.9-5
 transforward-GITPATH		:= git://git.planet-lab.org/transforward.git@transforward-0.1-3
-procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@master
+procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@procprotect-0.3-4
 ipfw-GITPATH                    := git://git.code.sf.net/p/dummynet/code@ipfw-20130423-1
 nodeupdate-GITPATH		:= git://git.planet-lab.org/nodeupdate.git@nodeupdate-0.5-10
 PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/tags/PingOfDeath-2.2-1
