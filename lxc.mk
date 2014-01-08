16991243df33aeaa7a8d8388a4feca64429236e9
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-01-08 08:39:45 +0100

    adding a missing dep for libvirt on f20 with this in place our libvirt still won’t build on f20, will try to use the stock version (1..1.3.2) instead

--- lxc.mk
+++ lxc.mk
@@ -159,7 +159,7 @@ endif
 # strictly speaking fuse-devel is not required anymore but we might wish to turn fuse back on again in the future
 ifeq "$(DISTRONAME)" "$(filter $(DISTRONAME),f18 f20)"
 libvirt-STOCK-DEVEL-RPMS += fuse-devel libssh2-devel dbus-devel numad 
-libvirt-STOCK-DEVEL-RPMS += systemd-devel libnl3-devel iptables-ipv6 
+libvirt-STOCK-DEVEL-RPMS += systemd-devel libnl3-devel iptables-ipv6 libgcrypt-devel
 endif
 libvirt-RPMFLAGS :=	--define 'packager PlanetLab'
 ALL += libvirt
