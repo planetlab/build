6a6805a6e764e37642fc7db90fb3f4e31e5ee039
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-01-08 08:40:02 +0100

    switch to using stock libvirt onf 20

--- lxc.mk
+++ lxc.mk
@@ -135,11 +135,11 @@ fprobe-ulog-SPEC := fprobe-ulog.spec
 ALL += fprobe-ulog
 IN_NODEIMAGE += fprobe-ulog
 
-#################### using our own libvirt on all distros for now
-#local_libvirt=false
-#ifeq "$(DISTRONAME)" "f16"
+#################### using our own libvirt on f18, and the stock version later on
+local_libvirt=false
+ifeq "$(DISTRONAME)" "f18"
 local_libvirt=true
-#endif
+endif
 
 ifeq "$(local_libvirt)" "true"
 #
