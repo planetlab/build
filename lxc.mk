8b59bc0fd97b6b2103311a18252d385006103d17
Mohamed Larabi <mohamed.larabi@inria.fr>
2014-02-07 17:21:26 +0100

    add libvirt-python-STOCK-DEVEL-RPMS

--- lxc.mk
+++ lxc.mk
@@ -182,6 +182,7 @@ ifeq "$(separate_libvirt_python)" "true"
 libvirt-python-MODULES := libvirt-python
 libvirt-python-SPEC    := libvirt-python.spec
 libvirt-python-BUILD-FROM-SRPM := yes
+libvirt-python-STOCK-DEVEL-RPMS += gnutls-utils nc pm-utils 
 libvirt-python-LOCAL-DEVEL-RPMS += libvirt-devel libvirt-docs libvirt-client
 libvirt-python-RPMFLAGS :=     --define 'packager PlanetLab'
 ALL += libvirt-python
