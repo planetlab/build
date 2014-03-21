8aaf2a42f56e2f99a0c61dce1cb9ccdff2e4ccf9
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-03-21 16:32:26 +0100

    Setting tag procprotect-0.4-3 guarded against some hypothetically possible errors

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -1,6 +1,6 @@
 lxc-userspace-GITPATH           := git://git.planet-lab.org/lxc-userspace.git@master
 transforward-GITPATH            := git://git.planet-lab.org/transforward.git@transforward-0.1-4
-procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@master
+procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@procprotect-0.4-3
 ipfw-GITPATH                    := git://git.code.sf.net/p/dummynet/code@ipfw-20130423-1
 comgt-SVNPATH			:= http://svn.onelab.eu/comgt/imports/0.3
 planetlab-umts-tools-GITPATH    := git://git.onelab.eu/planetlab-umts-tools.git@planetlab-umts-tools-0.7-1
