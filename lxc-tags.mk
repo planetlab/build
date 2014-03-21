672bb1e13b1127b5af4841ae4c41939523254791
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-03-21 16:33:55 +0100

    Setting tag transforward-0.1-5 harmless (changed default build kernel to the latest kernel in use)

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -1,5 +1,5 @@
 lxc-userspace-GITPATH           := git://git.planet-lab.org/lxc-userspace.git@master
-transforward-GITPATH            := git://git.planet-lab.org/transforward.git@transforward-0.1-4
+transforward-GITPATH            := git://git.planet-lab.org/transforward.git@transforward-0.1-5
 procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@procprotect-0.4-3
 ipfw-GITPATH                    := git://git.code.sf.net/p/dummynet/code@ipfw-20130423-1
 comgt-SVNPATH			:= http://svn.onelab.eu/comgt/imports/0.3
