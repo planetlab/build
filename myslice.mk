ee00241f79c64d2cd955f3db7ebe5cad2002a978
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-04-03 10:39:07 +0200

    myslice does not depend on manifold at build-time anymore

--- myslice.mk
+++ myslice.mk
@@ -7,6 +7,9 @@ myslice-MODULES := myslice
 myslice-SPEC := myslice.spec
 myslice-STOCK-DEVEL-RPMS := python-django
 myslice-STOCK-DEVEL-DEBS := python-django python-django-south
-myslice-LOCAL-DEVEL-DEBS := manifold
+# the -LOCAL-DEVEL-DEBS mechanism should work per se
+# however manifold itself depends on sfa so it was starting to be a lot to swallow
+# instead we have trimmed tweaked settings.py so it can be loaded in a build env.
+#myslice-LOCAL-DEVEL-DEBS := manifold
 ALL += myslice
 
