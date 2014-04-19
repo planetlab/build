1983f31ab5a347c9c3b07caa4afa4420a07c6efb
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-04-19 18:05:54 +0200

    all modules with pending changes

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -1,13 +1,13 @@
-lxc-userspace-GITPATH           := git://git.planet-lab.org/lxc-userspace.git@lxc-userspace-1.0-10
-transforward-GITPATH            := git://git.planet-lab.org/transforward.git@transforward-0.1-5
-procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@procprotect-0.4-3
+lxc-userspace-GITPATH           := git://git.planet-lab.org/lxc-userspace.git@master
+transforward-GITPATH            := git://git.planet-lab.org/transforward.git@master
+procprotect-GITPATH             := git://git.planet-lab.org/procprotect.git@master
 ipfw-GITPATH                    := git://git.code.sf.net/p/dummynet/code@ipfw-20130423-1
 comgt-SVNPATH			:= http://svn.onelab.eu/comgt/imports/0.3
 planetlab-umts-tools-GITPATH    := git://git.onelab.eu/planetlab-umts-tools.git@planetlab-umts-tools-0.7-1
 nodeupdate-GITPATH              := git://git.planet-lab.org/nodeupdate.git@nodeupdate-0.5-11
 PingOfDeath-SVNPATH		:= http://svn.planet-lab.org/svn/PingOfDeath/tags/PingOfDeath-2.2-1
 plnode-utils-GITPATH            := git://git.planet-lab.org/plnode-utils.git@plnode-utils-0.2-2
-nodemanager-GITPATH             := git://git.planet-lab.org/nodemanager.git@nodemanager-5.2-12
+nodemanager-GITPATH             := git://git.planet-lab.org/nodemanager.git@master
 #
 pl_sshd-SVNPATH			:= http://svn.planet-lab.org/svn/pl_sshd/tags/pl_sshd-1.0-11
 codemux-GITPATH			:= git://git.planet-lab.org/codemux.git@codemux-0.1-15
@@ -21,7 +21,7 @@ mom-GITPATH                     := git://git.planet-lab.org/mom.git@mom-2.3-5
 inotify-tools-GITPATH		:= git://git.planet-lab.org/inotify-tools.git@inotify-tools-3.13-2
 openvswitch-GITPATH		:= git://git.planet-lab.org/openvswitch.git@openvswitch-1.2-1
 vsys-GITPATH			:= git://git.planet-lab.org/vsys.git@vsys-0.99-3
-vsys-scripts-GITPATH            := git://git.planet-lab.org/vsys-scripts.git@vsys-scripts-0.95-48
+vsys-scripts-GITPATH            := git://git.planet-lab.org/vsys-scripts.git@master
 bind_public-GITPATH             := git://git.planet-lab.org/bind_public.git@bind_public-0.1-2
 sliver-openvswitch-GITPATH      := git://git.onelab.eu/sliver-openvswitch.git@sliver-openvswitch-2.1.90-2
 plcapi-GITPATH                  := git://git.planet-lab.org/plcapi.git@plcapi-5.3-2
