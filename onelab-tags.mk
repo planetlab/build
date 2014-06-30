3f9b99bdbf83f4e8a3b266c564e6253da2c0a7f3
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-06-30 16:27:14 +0200

    sliceimage with systemd

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -45,7 +45,7 @@ rvm-ruby-GITPATH                := git://git.onelab.eu/rvm-ruby.git@rvm-ruby-1.2
 oml-GITPATH                     := git://git.onelab.eu/oml.git@oml-2.6.1-1
 ###
 bootcd-GITPATH                  := git://git.onelab.eu/bootcd.git@bootcd-5.2-4
-sliceimage-GITPATH              := git://git.onelab.eu/sliceimage.git@sliceimage-5.1-7
+sliceimage-GITPATH              := git://git.onelab.eu/sliceimage.git@master
 nodeimage-GITPATH               := git://git.onelab.eu/nodeimage.git@nodeimage-5.2-3
 myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.3-2
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
