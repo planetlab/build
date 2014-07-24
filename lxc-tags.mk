cf05676fd21c89f159f4181532e45e557633c36d
Thomas Dreibholz <dreibh@simula.no>
2014-07-24 15:38:02 +0200

    Updated tags.

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -1,6 +1,6 @@
 lxc-userspace-GITPATH           := git://git.onelab.eu/lxc-userspace.git@lxc-userspace-1.0-12
-transforward-GITPATH            := git://git.onelab.eu/transforward.git@transforward-0.1-7
-procprotect-GITPATH             := git://git.onelab.eu/procprotect.git@procprotect-0.4-5
+transforward-GITPATH            := git://git.onelab.eu/transforward.git@master
+procprotect-GITPATH             := git://git.onelab.eu/procprotect.git@master
 ipfw-GITPATH                    := git://git.code.sf.net/p/dummynet/code@155b6cd31089b4763297d579e9c9945393f00c40
 comgt-GITPATH			:= git://git.onelab.eu/comgt.git@0.3
 planetlab-umts-tools-GITPATH    := git://git.onelab.eu/planetlab-umts-tools.git@planetlab-umts-tools-0.7-1
