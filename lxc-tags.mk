4465864e81fa6eaa69c13dd426dcb493c6263360
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2014-09-15 16:16:34 +0200

    Setting tag sfa-3.1-11 various tweaks for the openlab demo first stab at uploading onto a pypi (for pip install sfa)

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -49,6 +49,6 @@ myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.3-2
 
 #
 sfa-BRANCH			:= geni-v3
-sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-3.1-10
+sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-3.1-11
 #
 tests-GITPATH                   := git://git.onelab.eu/tests.git@master
