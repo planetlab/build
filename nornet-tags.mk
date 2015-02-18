5424fcc13b7bd8492ffa01a62fbd53c456ad1fa6
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-02-18 12:48:51 +0100

    Setting tag tests-5.3-9 add tests for sfa and empty rspecs

--- nornet-tags.mk
+++ nornet-tags.mk
@@ -87,4 +87,4 @@ myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.3-3
 sfa-BRANCH			:= geni-v3
 sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-3.1-13
 #
-tests-GITPATH                   := git://git.onelab.eu/tests.git@tests-5.3-8
+tests-GITPATH                   := git://git.onelab.eu/tests.git@tests-5.3-9
