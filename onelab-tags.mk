75d4cc85a170771ebe34c1c927a883bbf9623d1e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-03-02 21:20:29 +0100

    use latest sfa no matter what

--- onelab-tags.mk
+++ onelab-tags.mk
@@ -51,8 +51,7 @@ myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.3-3
 DistributedRateLimiting-SVNPATH	:= http://svn.planet-lab.org/svn/DistributedRateLimiting/tags/DistributedRateLimiting-0.1-1
 
 #
-sfa-BRANCH			:= geni-v3
-sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-3.1-13
+sfa-GITPATH                     := git://git.onelab.eu/sfa.git@master
 #
 # locating the right test directory - see make tests_gitpath
 tests-GITPATH                   := git://git.onelab.eu/tests.git@master
