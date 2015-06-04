ca2d96d707f7d1cbde22b82a0e8785104195f97e
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2015-06-04 14:18:29 +0200

    Setting tag sfa-3.1-16 added a new builtin column 'name' for authorities in the sfa registry this is kept in sync with MyPLC's site names when relevant sfa update -t authority thus now has a new -n/--name option sfi register or update can specify record type on only 2 characters (au, us, no, or sl) reviewed Describe and Allocate wrt slice tags for a PL AM: Describe now exposes all slice tags with a 'scope' being 'sliver' or 'slice' Allocate now by default ignores incoming slice tags Allocate's options can mention 'pltags' among 'ignore', 'append', 'sync' default being 'ignore' in 'ignore' mode, slice tags are unchanged in the PL db in 'append' mode, slice tags from the rspec are added to the db unless they are already present in 'sync' mode, the code attempts to leave the PL db in sync with the tags provided in rspec; this can be dangerous and is thus no longer the default behaviour

--- lxc-tags.mk
+++ lxc-tags.mk
@@ -48,6 +48,6 @@ nodeimage-GITPATH               := git://git.onelab.eu/nodeimage.git@nodeimage-5
 myplc-GITPATH                   := git://git.onelab.eu/myplc.git@myplc-5.3-3
 
 #
-sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-3.1-15
+sfa-GITPATH                     := git://git.onelab.eu/sfa.git@sfa-3.1-16
 #
 tests-GITPATH                   := git://git.onelab.eu/tests.git@master
