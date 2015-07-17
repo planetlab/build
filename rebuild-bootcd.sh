a19ebffc6eb0a882ce8bdfbd3804e78b10744181
Andy Bavier <acb@cs.princeton.edu>
2015-07-17 13:09:06 -0400

    More fixes

--- rebuild-bootcd.sh
+++ rebuild-bootcd.sh
@@ -4,5 +4,6 @@ make bootcd-clean
 make bootcd-source
 
 cp /build/mirroring/centos6/yum.repos.d/building.repo.in MODULES/build/mirroring/centos6/yum.repos.d/
+cp /build/config.mlab/mlab.mirrors MODULES/build/config.mlab/
 cp /build/config.mlab/yumexclude.pkgs MODULES/build/config.mlab/yumexclude.pkgs
 make bootcd
