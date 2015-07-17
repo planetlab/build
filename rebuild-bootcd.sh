852a9c0464f14e2ad593afed3b1a1f13c9377421
Andy Bavier <acb@cs.princeton.edu>
2015-07-17 12:41:12 -0400

    Path fixes

--- rebuild-bootcd.sh
+++ rebuild-bootcd.sh
@@ -3,7 +3,6 @@
 make bootcd-clean
 make bootcd-source
 
-cp building.repo.in MODULES/build/mirroring/centos6/yum.repos.d/building.repo.in
-cp build.common MODULES/build/build.common
-cp config.mlab/yumexclude.pkgs MODULES/build/config.mlab/yumexclude.pkgs
+cp /build/mirroring/centos6/yum.repos.d/building.repo.in MODULES/build/mirroring/centos6/yum.repos.d/
+cp /build/config.mlab/yumexclude.pkgs MODULES/build/config.mlab/yumexclude.pkgs
 make bootcd
