eadf669968c734c25e5d63d8fd4cedf3c8823fb0
functor <mw>
2016-06-21 14:23:55 -0400

    Actually build all bootcd packages.

--- rebuild-bootcd.sh
+++ rebuild-bootcd.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 make bootcd-clean
+make bootcdR420-clean
+make bootcdR630-clean
+
 make bootcd-source
+make bootcdR420-source
+make bootcdR630-source
 
 cp /build/mirroring/centos6/yum.repos.d/building.repo.in MODULES/build/mirroring/centos6/yum.repos.d/
 cp /build/config.mlab/mlab.mirrors MODULES/build/config.mlab/
 cp /build/config.mlab/yumexclude.pkgs MODULES/build/config.mlab/yumexclude.pkgs
+
 make bootcd
+make bootcdR420
+make bootcdR630
